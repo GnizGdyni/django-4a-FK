a9ebeeb8f152410c0f6bfeaa510e765d68b948a4
Filip K <klassafilip@gmail.com>
2024-03-01 08:37:17 +0100

    modele

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/", views.index, name="index"),
+    path("", views.index, name="index"),
+    path("counter/", views.counter, name="c"),
+    path("basic/", views.basic, name="b"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,4 +4,31 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse('''
+
+    <button onclick="inc">Hello World</button>
+    <br><br>
+    <input></input>
+    <br><br>
+    <select></select>
+        <script>
+    var inc = function() {
+        alert("yooo")
+    }
+
+    </script>
+    ''')
+
+def counter(request):
+    text_file = open("./templates/fv.html","r")
+    page = text_file.read()
+    text_file.close()
+    # page = "fggdd"
+    print(page)
+    return HttpResponse(page)
+
+def basic(request):
+    text_file = open("./templates/home.html","r")
+    page = text_file.read()
+    text_file.close()
+    return HttpResponse(page)
